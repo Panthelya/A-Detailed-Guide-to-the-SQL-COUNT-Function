9f014899f6a9bd4dcd3b27dede52ef8e49eb2911
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-02-16 09:42:19 +0100

    Add files via upload

---
+++ directors_DB.sql
@@ -0,0 +1,20 @@
+CREATE TABLE directors (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR (50),
+	date_of_birth DATE,
+	date_of_death DATE,
+	place_of_birth VARCHAR (50),
+	country_of_birth VARCHAR (50)
+	
+);
+INSERT INTO directors (id, first_name, last_name, date_of_birth, date_of_death, place_of_birth, country_of_birth) VALUES (1, 'Ingmar', 'Bergman', '1918-07-14', '2007-07-30', 'Uppsala', 'Sweden');
+INSERT INTO directors (id, first_name, last_name, date_of_birth, date_of_death, place_of_birth, country_of_birth) VALUES (2, 'Lynne', 'Ramsay', '1969-12-05', NULL, 'Glasgow', 'Scotland');
+INSERT INTO directors (id, first_name, last_name, date_of_birth, date_of_death, place_of_birth, country_of_birth) VALUES (3, 'Alejandro', 'Jodorowsky', '1929-02-07', NULL, 'Tocophilla', 'Chille');
+INSERT INTO directors (id, first_name, last_name, date_of_birth, date_of_death, place_of_birth, country_of_birth) VALUES (4, 'Agnès', 'Varda', '1928-05-30', '2019-03-29', 'Brussels', 'Belgium');
+INSERT INTO directors (id, first_name, last_name, date_of_birth, date_of_death, place_of_birth, country_of_birth) VALUES (5, 'Pedro', 'Almodóvar', '1949-09-25', NULL, 'Calzada de Calatrava', 'Spain');
+INSERT INTO directors (id, first_name, last_name, date_of_birth, date_of_death, place_of_birth, country_of_birth) VALUES (6, 'Chloé', 'Zhao', '1982-03-31', NULL, 'Beijing', 'China');
+INSERT INTO directors (id, first_name, last_name, date_of_birth, date_of_death, place_of_birth, country_of_birth) VALUES (7, 'Jordan', 'Peele', '1979-02-21', NULL, 'New York City', 'USA');
+INSERT INTO directors (id, first_name, last_name, date_of_birth, date_of_death, place_of_birth, country_of_birth) VALUES (8, 'Céline', 'Sciamma', '1978-11-12', NULL, 'Pontoise', 'France');
+INSERT INTO directors (id, first_name, last_name, date_of_birth, date_of_death, place_of_birth, country_of_birth) VALUES (9, 'Jean-Luc', 'Godard', '1930-12-03', '2022-09-13', 'Paris', 'France');
+INSERT INTO directors (id, first_name, last_name, date_of_birth, date_of_death, place_of_birth, country_of_birth) VALUES (10, 'Stanley', 'Kubrick', '1928-07-26', '1999-03-07', 'New York City', 'USA');
